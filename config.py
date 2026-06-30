827a77caab30f3e596cf8e0990d42d1f70748a63
Rishith Namani <rishith030605@gmail.com>
2026-06-30 19:30:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+import snowflake.connector
+
+load_dotenv()
+
+def get_connection():
+    conn = snowflake.connector.connect(
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+        database=os.getenv("SNOWFLAKE_DATABASE"),
+        schema=os.getenv("SNOWFLAKE_SCHEMA")
+    )
+    return conn
